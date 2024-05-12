1f182f7738a218a7744658337fd77d917f02cfa0
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-05-12 13:27:43 +0300

    add code without main console code

---
+++ main.py
@@ -0,0 +1,32 @@
+from config import config
+from src.dbmanager import DBManager
+from src.utils import get_hh_data, save_data_to_database, create_database
+
+
+def main():
+    employers_id = [
+        '3127',  # ПАО Мегафон
+        '2748',  # ПАО Ростелеком
+        '665467',  # ГринАтом
+        '84585',  # Avito
+        '1942330',  # Пятерочка
+        '3707941',  # Цитадель
+        '1057',  # Лаборатория Касперского
+        '1740',  # Яндекс
+        '87021',  # WILDBERRIES
+        '15478',  # VK
+    ]
+
+    params = config()
+
+    data = get_hh_data(employers_id)
+    create_database('hh', params)
+    save_data_to_database(data, 'hh', params)
+
+    db = DBManager(params)
+
+    # Здесь будет основной код работы консольной программы
+
+
+if __name__ == "__main__":
+    main()
