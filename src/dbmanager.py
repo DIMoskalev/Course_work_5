5759a4b5c2863d2578f5d4281813403c2b1fb01c
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-05-12 20:04:53 +0300

    add get_companies_and_vacancies_count and

--- src/dbmanager.py
+++ src/dbmanager.py
@@ -1,25 +1,62 @@
+import psycopg2
+
+
 class DBManager():
 
-    def __init__(self):
-        pass
+    def __init__(self, params: dict, dbname: str = 'hhru'):
+        self.conn = psycopg2.connect(dbname=dbname, **params)
+        self.cur = self.conn.cursor()
 
     def get_companies_and_vacancies_count(self):
         """Получает список всех компаний и количество вакансий у каждой компании"""
-        pass
+        with self.conn:
+            self.cur.execute(
+                """
+                SELECT employer_name, COUNT(vacancy_id)
+                FROM employers
+                JOIN vacancies USING(employer_id)
+                GROUP BY employer_name;
+                """
+            )
+            return self.cur.fetchall()
 
     def get_all_vacancies(self):
         """Получает список всех вакансий с указанием названия компании, названия вакансии и зарплаты,
         а также ссылки на вакансию"""
-        pass
+        with self.conn:
+            self.cur.execute(
+                """
+                SELECT employer_name, vacancy_name, salary_from, salary_to
+                currency, vacancies.url
+                FROM vacancies
+                JOIN employers USING(employer_id);
+                """
+            )
+            return self.cur.fetchall()
 
     def get_avg_salary(self):
         """Получает среднюю зарплату по вакансиям"""
-        pass
+        with self.conn:
+            self.cur.execute(
+                """
+                
+                """
+            )
 
     def get_vacancies_with_higher_salary(self):
         """Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям"""
-        pass
+        with self.conn:
+            self.cur.execute(
+                """
+                
+                """
+            )
 
     def get_vacancies_with_keyword(self):
         """Получает список всех вакансий, в названии которых содержатся переданные в метод слова, например python"""
-        pass
+        with self.conn:
+            self.cur.execute(
+                """
+                
+                """
+            )
